cb4619db6d4595d06f52f6074a6742847ded1cbc
hao <jiun8631@gmail.com>
2025-08-17 09:34:59 +0800

    sitemap多條/

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://booo.dpdns.org/';  // 替換成你的網站域名
+  const baseUrl = 'https://booo.dpdns.org';  // 替換成你的網站域名
 
   return [
     {
